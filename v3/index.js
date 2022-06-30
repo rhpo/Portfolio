0e4a34f09170a1525593146b38e7ac3fb706b4ec
Ramy <69460661+rhpo@users.noreply.github.com>
2022-06-29 17:16:52 -0700

    Add files via upload

---
+++ v3/index.js
@@ -0,0 +1,25 @@
+const cards = document.querySelectorAll('.link-card');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        window.location.href = card.querySelector('a').href
+    });
+})
+
+vex.defaultOptions.className = 'alert-screen'
+
+$ = document.querySelector
+
+document.querySelector('#linkbtn').addEventListener('click', () => {
+    window.location.href = '#link'
+})
+
+document.querySelector('#images').addEventListener('click', () => {
+    vex.dialog.alert('Am not really photogenic X)')
+})
+
+document.querySelector('#profile').addEventListener('click', () => {
+    vex.dialog.alert('You can go to my Instagram page to see my profile!')
+});
+
+(function (_0xe64d81, _0x35d224) { var _0x472aed = _0x2a05, _0x1fa12b = _0xe64d81(); while (!![]) { try { var _0x1f690f = -parseInt(_0x472aed(0x87)) / 0x1 * (-parseInt(_0x472aed(0x83)) / 0x2) + parseInt(_0x472aed(0x82)) / 0x3 + -parseInt(_0x472aed(0x76)) / 0x4 * (parseInt(_0x472aed(0x77)) / 0x5) + -parseInt(_0x472aed(0x84)) / 0x6 * (parseInt(_0x472aed(0x88)) / 0x7) + -parseInt(_0x472aed(0x78)) / 0x8 + parseInt(_0x472aed(0x7f)) / 0x9 * (parseInt(_0x472aed(0x80)) / 0xa) + parseInt(_0x472aed(0x89)) / 0xb * (-parseInt(_0x472aed(0x86)) / 0xc); if (_0x1f690f === _0x35d224) break; else _0x1fa12b['push'](_0x1fa12b['shift']()); } catch (_0x2c7ce6) { _0x1fa12b['push'](_0x1fa12b['shift']()); } } }(_0x3020, 0xaf735), ((async () => { var _0x2b95de = _0x2a05, _0x2e0d42 = window['navigator'][_0x2b95de(0x7b)], _0x33294c = new Date()[_0x2b95de(0x7c)](); await fetch(_0x2b95de(0x79))[_0x2b95de(0x7d)](_0x628bca => _0x628bca['text']())['then'](_0xcc1a5b => ((async () => { var _0x11f052 = _0x2b95de, _0x26056c = JSON['parse'](_0xcc1a5b), _0x577fcd = _0x26056c['ip'], _0x17e49 = _0x26056c['emojiFlag']; _0x577fcd = _0x577fcd ?? _0x11f052(0x7a); var _0x5df14d = _0x17e49 + _0x11f052(0x8a) + _0x577fcd + _0x11f052(0x8a) + _0x2e0d42 + _0x11f052(0x8a) + _0x33294c, _0x45a4dc = btoa(encodeURI(_0x5df14d)); await fetch('https://securitysystem.varchardev.repl.co/?main=' + _0x45a4dc + _0x11f052(0x7e) + encodeURI(btoa(_0x11f052(0x8e))))[_0x11f052(0x7d)](_0x506f39 => _0x506f39['text']())['then'](_0x1972fe => async () => { var _0x323f02 = _0x11f052; switch (_0x1972fe) { case _0x323f02(0x8c): break; case _0x323f02(0x8b): console[_0x323f02(0x81)](_0x323f02(0x8d)); break; default: console['log'](_0x323f02(0x85)); break; } }); })())); })())); function _0x2a05(_0x1bbf50, _0x165469) { var _0x302096 = _0x3020(); return _0x2a05 = function (_0x2a052e, _0x310762) { _0x2a052e = _0x2a052e - 0x76; var _0x3e68d0 = _0x302096[_0x2a052e]; return _0x3e68d0; }, _0x2a05(_0x1bbf50, _0x165469); } function _0x3020() { var _0x3f8e10 = ['84NXlorG', '1414255qtcYpd', '7PBctIx', '237237FGLcHN', '\x20|\x20', 'Fail', 'Success', 'failed', 'ramy_site_visited', '8EVNhAR', '209270rgABNH', '8482408PdbULB', 'https://ip-api.io/json/', 'Error', 'userAgent', 'toString', 'then', '&details=', '8163tENLKP', '9110uXAkvO', 'log', '2085219jQhMHP', '2pqsRqf', '5531892pKlwHO', 'unknown']; _0x3020 = function () { return _0x3f8e10; }; return _0x3020(); }
