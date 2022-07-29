21e8fe115be97ce7cd2427ddd653834a28a76542
Ramy <69460661+rhpo@users.noreply.github.com>
2022-07-29 16:06:53 +0200

    Create index.js

---
+++ more/color/index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<head>
+  <title>Color</title>
+</head>
+
+<body />
+
+<script src="https://chir.ag/projects/ntc/ntc.js" type="module" defer />
+<script>
+   let { color } = new URLSearchParams(location.search);
+   !color && (() => color = '#' + Math.floor(Math.random()*16777215).toString(16))();
+   var string = ntc.name(color)[1];
+   document.title += `: ${string}`;
+   document.documentElement.style.background = document.body.style.background = color;
+</script>
